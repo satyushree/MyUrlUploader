fee766430b9ba369edf73ee1e8a6f5d8e50146dc
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-15 11:24:50 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -16,9 +16,13 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
+    #
+    from script import script
 
+    from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 # the Strings used for this "thing"
-from translation import Translation
+#from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -30,8 +34,8 @@ def GetExpiryDate(chat_id):
     Config.AUTH_USERS.add(683538773)
     return expires_at
 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
+#
+"""@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
 async def help_user(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/help")
@@ -41,8 +45,19 @@ async def help_user(bot, update):
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
-    )
+    )"""
 
+@Client.on_message(filters.command(["help"]))
+def help_user(bot, update):
+
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.HELP_USER,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
 async def get_me_info(bot, update):
@@ -58,8 +73,22 @@ async def get_me_info(bot, update):
         reply_to_message_id=update.message_id
     )
 
+@Client.on_message(filters.command(["start"]))
+def send_start(bot, update):
+    
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ CHANNEL ⭕️", url="https://t.me/TroJanzHEX")], [InlineKeyboardButton(text="😇 SUPPORT", url="https://t.me/TroJanzSupport"),
+                                                    InlineKeyboardButton(text="SHARE ♐️", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40Zee5HEXBot%2A%2A%20%F0%9F%94%A5")]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+
+#
+"""@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
 async def start(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/start")
@@ -67,16 +96,28 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
-    )
+    )"""
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+#
+"""@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
 async def upgrade(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/upgrade")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )"""
+    
+    @Client.on_message(filters.command(["upgrade"]))
+def upgrade(bot, update):
+
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.UPGRADE_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
